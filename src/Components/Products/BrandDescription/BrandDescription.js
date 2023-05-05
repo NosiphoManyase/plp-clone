a367bfd67320f854a4b6a85ac7ddabb625f7b2f3
Nosipho <manyasenosipho@gmail.com>
2023-05-05 11:48:12 +0200

    update styling for product display section

--- src/Components/Products/BrandDescription/BrandDescription.js
+++ src/Components/Products/BrandDescription/BrandDescription.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import nikeLogo from '../../../assets/logo-frame.png'
-import './brand-description.scss'
 import dropdown from '../../../assets/downTriangle.png'
 
 const BrandDescription = () => {
   return (
-    <div className='brand-info'>
+    <div className='flex'>
         <div>
           <img src={nikeLogo} alt=''/>
         </div>
-        <div className='brand-content'>
-            <div className='brand-header'>
+        <div className='mx-4'>
+            <div className='text-[22px] font-semibold mt-2'>
                 Nike
             </div>
             <div>
-              <p className='brand-description'>
+              <p className='text-sm'>
                 Our mission is what drives us to do everything possible to expand human potential. We do that by creating groundbreaking sport innovations, by making our products more sustainably, by building a creative and diverse global team and by making a positive impact in communities where we live and work.
               </p>
-              <a className='more-cta'>More information 
-                <img src={dropdown} alt=''/>
+              <a className='text-xs font-semibold leading-[14px]'>More information 
+                <img className='ml-3.5' src={dropdown} alt=''/>
               </a>
             </div>
         </div>
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -3,53 +3,55 @@ import BrandDescription from './BrandDescription/BrandDescription'
 import { products } from '../../data/data' 
 import addToWishlist from '../../assets/wishlistIcon.png'
 import dropdown from '../../assets/downTriangle.png'
-import './product-list.scss'
+// import './product-list.scss'
 import RecentlyViewed from './RecentlyViewed/RecentlyViewed'
 
 const Products = () => {
   return (
-    <div className='product-section'>
+    <div className='w-[902px] mt-5 mb-[15px]'>
         <BrandDescription />
 
-        <div className='product-count'>
-          200 <span>items</span>
+        <div className='my-[15px] text-xs font-bold'>
+          200 <span className='text-[#919191]'>items</span>
         </div>
 
         <div className='product-list'>
           {products.map(product => (
           <div className='item' key={product.name.replace(/\s/g, '-')}>
-            <div className='item-display'>
+            <div className='relative'>
               <img src={addToWishlist} alt='' className='wishlist-icon'/>
               <img src={product.image} alt='' className='item-img'/>
-              {product.sale && <div className='sale-widget'>SALE</div>}
-              {product.new && <div className='new-widget'>NEW</div>}
-              {product.promo && <div className='promo-widget'>PROMO</div>}
+              {product.sale && <div className='widget w-[38px] h-5 bg-[#FF3933]'>SALE</div>}
+              {product.new && <div className='widget w-[50px] h-5 bg-[#040404]'>NEW</div>}
+              {product.promo && <div className='widget w-[50px] h-5 bg-[#4646E8]'>PROMO</div>}
             </div>
             <div className='item-info'>
               <p className='item-name'>{product.name}</p>
               <p className='item-brand'>{product.brand}</p>
               <p className='item-price'>{product.sale && 'Sale Price: '}
-                <span className={product.sale?'sale-price': undefined}>{product.price}</span>
+                <span className={product.sale?'text-[#FF3933] line-through': undefined}>{product.price}</span>
                 {product.promo && ' - Range'}
               </p>
-              <p className='item-swatch'></p>
+              <p className='w-[13px] h-[13px] bg-[#766EFF]'></p>
             </div>
           </div>
         ))}
         </div>
 
-        <div className='load-more'>
+        <div className='flex flex-col items-center py-4 '>
           <div>
-            <p>You've viewed 10 out of 80 products</p>
-            <div className='full-bar'>
-              <div className='progress-bar'></div>
+            <p className='font-semibold text-[10px] text-[#919191] pb-2'>You've viewed 10 out of 80 products</p>
+            <div className='w-[189.6px] h-[4px] bg-trueGray-200 mb-4'>
+              <div className='h-[4px] w-[143px] bg-trueGray-950'></div>
             </div>
           </div>
-          <button>Load more</button>
+          <button className='w-[120px] h-[48px] border-visible border border-trueGray-950 font-semibold text-trueGray-950'>
+            Load more
+          </button>
         </div>
         
-        <div className='seo-block'>
-            <h4>Title for lorem ipsum</h4>
+        <div className=''>
+            <h4 >Title for lorem ipsum</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...</p>
             <a>See more <img src={dropdown} alt=''/></a>
         </div>
